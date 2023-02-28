5b3ff738233e57e1905fcd8c2fd9e9292a881848
周揚凱 <brian950703@gmail.com>
2023-02-28 15:13:19 +0800

    third draft

--- src/components/Total.js
+++ src/components/Total.js
@@ -3,15 +3,28 @@ import { Context } from "../Context";
 import data from "../data";
 
 export default function Total() {
-  const [items] = useContext(Context);
-
-  const totalPrice = Object.keys(items).reduce((acc, curr) => {
+    const [items] = useContext(Context);
+    const weekday = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+    const d = new Date();
+    let day = weekday[d.getDay()];
     
-    const [group, item] = curr.split("-");
-    
-    const amount = items[curr] * data[group].menu[item].price;
-    return acc + amount;
-  }, 0);
+    const totalPrice = Object.keys(items).reduce((acc, curr) => {
+        const [group, item] = curr.split("-");
+        const amount = items[curr] * data[group].menu[item].price;
+        if(day==="Tuesday" && group==="miso"){
+            return acc+(amount*0.8);
+        }
+        else if(day==="Wednesday" && group==="pork"){
+            return acc+(amount*0.8);
+        }
+        else if(day==="Thursday" && group==="soysauce"){
+            return acc+(amount*0.8);
+        }
+        else if(day==="Friday" && group==="spicy"){
+            return acc+(amount*0.8);
+        }
+        return acc+amount;
+    }, 0);
 
   return (
     <div className="total">
